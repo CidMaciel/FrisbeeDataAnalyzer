18485767cc79f82bea425699df8ca1396914a77e
Cid <cmlw2023@mymail.pomona.edu>
2025-05-14 21:50:00 -0700

    added comments to player impact

--- PlayerImpact.java
+++ PlayerImpact.java
@@ -1,16 +1,33 @@
+/**
+ * @Author Cid Maciel and Emmett Levine
+ */
 public class PlayerImpact {
     private String name;
     private double impact;
 
+    /**
+     * constructor, stores the player's name and impact
+     * 
+     * @param name
+     * @param impact
+     */
     public PlayerImpact(String name, double impact) {
         this.name = name;
         this.impact = impact;
     }
     
+    /**
+     * 
+     * @return the name of the player
+     */
     public String getName() {
         return name;
     }
     
+    /**
+     * 
+     * @return the impact of the player
+     */
     public double getImpact() {
         return impact;
     }
